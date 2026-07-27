5bcc23dddd0a4c06b9436df48bf8dd23402f4d2e
Hybrid Tecnologia Inteligente <contato@hybridtech.com.br>
2026-07-27 20:13:42 -0300

    feat(kernel): export public api

---
+++ packages/kernel/src/index.ts
@@ -0,0 +1,7 @@
+export * from './bootstrap/BootContext.js';
+export * from './bootstrap/BootStage.js';
+export * from './bootstrap/BootReport.js';
+export * from './bootstrap/BootPipeline.js';
+export * from './bootstrap/BootEngine.js';
+export * from './lifecycle/KernelState.js';
+export * from './lifecycle/LifecycleManager.js';
