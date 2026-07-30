ff9ef335c9a7dbfc22999d2e76892a4ec36b3f8e
Hybrid Tecnologia Inteligente <contato@hybridtech.com.br>
2026-07-30 11:45:19 -0300

    feat(kernel): export KernelContext

--- packages/kernel/src/index.ts
+++ packages/kernel/src/index.ts
@@ -3,5 +3,6 @@ export * from './bootstrap/BootStage.js';
 export * from './bootstrap/BootReport.js';
 export * from './bootstrap/BootPipeline.js';
 export * from './bootstrap/BootEngine.js';
+export * from './kernel/KernelContext.js';
 export * from './lifecycle/KernelState.js';
 export * from './lifecycle/LifecycleManager.js';
